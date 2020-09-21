8a8cad8f5d31bf150c48901665a3a374f0e6f35f
huylinh620 <huylinh620@gmal.com>
2020-09-21 18:17:24 +0700

    create function format Number

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import formatCurrency from '../util';
 
 export default class Products extends Component {
   render() {
@@ -15,7 +16,7 @@ export default class Products extends Component {
                 </div>
                 <div className="product-price">
                   <div>
-                    {product.price}
+                    {formatCurrency(product.price)}
                   </div>
                   <button className="button-primary">Add do Cart</button>
                 </div>
