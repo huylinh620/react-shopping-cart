1a097e85d100b097161873ae61f7e5b74ba4e92b
huylinh620 <huylinh620@gmal.com>
2020-09-24 12:39:14 +0700

    manage orders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Products from './components/Products';
-import Filter from './components/Filter';
-import Cart from './components/Cart';
 import store from './store';
 import {Provider} from 'react-redux';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import AdminScreen from './screens/AdminScreen';
+import HomeScreen from './screens/HomeScreen';
 
 import './App.css';
 
@@ -12,23 +12,19 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-      <div className="grid-container">
-        <header className="app-header">
-        <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-              <Filter></Filter>
-              <Products></Products>
-            </div>
-            <div className="sidebar">
-              <Cart></Cart>
-            </div>
-          </div>
-        </main>
-        <footer>All right is reserved</footer>
-      </div>
+        <BrowserRouter>
+        <div className="grid-container">
+          <header className="app-header">
+            <Link to="/">React Shopping Cart</Link>
+            <Link to="/admin">Admin</Link>
+          </header>
+          <main>
+            <Route path="/admin" component={AdminScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+          </main>
+          <footer>All right is reserved.</footer>
+        </div>
+      </BrowserRouter>
       </Provider>
     );
   }
